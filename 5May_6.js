000cd4e63b6c6c0e7470cc85cfa19e5734e2f23f
Bishama Irfan <bishamairfann10@gmail.com>
2022-05-05 14:15:43 +0200

    initial commit

--- 5May_6.js
+++ 5May_6.js
@@ -7,11 +7,10 @@ If he doesn't get 10 hoops, return the string "Keep at it until you get it". */
 
 function hoopCount (n) {
     //your code goes here  
-   if (n > 10) {
+   if (n >= 10) {
      return 'Great, now move on to tricks'
    }
    else {
      return 'Keep at it until you get it'
    }
  }
-
