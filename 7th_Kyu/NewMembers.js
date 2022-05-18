79311fe4555bd46a178d15596338da8844a96638
Bishama Irfan <bishamairfann10@gmail.com>
2022-05-18 20:36:03 +0200

    Categorize golf members

--- 7th_Kyu/NewMembers.js
+++ 7th_Kyu/NewMembers.js
@@ -8,3 +8,7 @@ Input will consist of a list of pairs. Each pair contains information for a sing
 Output
 Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category. */
 
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
