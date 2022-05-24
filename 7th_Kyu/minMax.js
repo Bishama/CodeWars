074f58d20d91a96e2e3033f0d02194d08254d2ab
Bishama Irfan <bishamairfann10@gmail.com>
2022-05-24 20:52:37 +0200

    Find min and max i array of integers

---
+++ 7th_Kyu/minMax.js
@@ -0,0 +1,8 @@
+/* Write a function that returns both the minimum and maximum number of the given list/array. */
+
+function minMax(arr){
+    // P -> array of integers
+    // R -> array containing min, max
+    
+    return [Math.min(...arr), Math.max(...arr)]
+  }
