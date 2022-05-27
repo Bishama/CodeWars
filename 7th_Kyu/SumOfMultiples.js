0e7d0337d9186b38deae815a0c6bd17257b3b715
Bishama Irfan <bishamairfann10@gmail.com>
2022-05-27 22:07:14 +0200

    Sum of multiples of 3 and 5

---
+++ 7th_Kyu/SumOfMultiples.js
@@ -0,0 +1,11 @@
+/* Upto and including n, this function will return the sum of all multiples of 3 and 5. */
+
+function findSum(n) {
+    var result = 0;
+    for(var i = 0; i <= n; i++) {
+      if(i % 3 === 0 || i % 5 === 0) {
+        res += i;
+      }
+    }
+    return result;
+  }
