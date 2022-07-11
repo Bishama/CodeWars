fed2d6bcd8cc81acd26c2aba367bebc2a51a87c9
Bishama Irfan <bishamairfann10@gmail.com>
2022-07-11 15:19:23 +0200

    Check for jumping numbers

---
+++ 7th_Kyu/JumpingNumbers.js
@@ -0,0 +1,25 @@
+function jumpingNumber(n){
+    //P => integer
+    //R => string
+    let number = n.toString();
+    //If length of n is 1 return jumping
+    if (number.length == 1){
+      return "Jumping!!"
+    }
+    // If length is greater than 1
+    if (number.length > 1){
+      //Check if the difference is 1 or -1 between adjacent digits
+      for (let i=0; i<number.length; i++){
+        //If the difference is not 1 or -1 return Not
+        if(Math.abs(number[i] - number[i+1]) !== 1){
+          return "Not!!"
+          break
+        }
+        return "Jumping!!"
+        
+      }
+    }
+      
+  
+    
+  }
