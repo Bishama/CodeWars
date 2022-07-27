e3663164030c408df0ddd6996ac5d296175e8f15
Bishama Irfan <bishamairfann10@gmail.com>
2022-07-27 22:45:18 +0200

    Maximum difference between successive elements

---
+++ 7th_Kyu/MaxGap.js
@@ -0,0 +1,12 @@
+/*Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form */
+
+function maxGap (numbers){
+ let num = numbers.sort((a,b) => a - b)
+ let sum = num[1] - num[0]
+ for(i=0; i < num.length; i++){
+    if(num[i+1] - num[i] > sum){
+      sum = num[i+1] - num[i]
+    }
+  }
+ return sum
+}
