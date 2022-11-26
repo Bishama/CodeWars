f70f5ff115e18d61c564816aaa39037444c8cb27
Bishama Irfan <bishamairfann10@gmail.com>
2022-11-26 10:45:29 +0000

    Filter for unique character and integers

---
+++ 6th_Kyu/UniqueOrder.js
@@ -0,0 +1,16 @@
+/* Implement the function unique_in_order which takes as argument a sequence and returns a list of items
+ without any elements with the same value next to each other and preserving the original order of elements. */
+
+ var uniqueInOrder=function(iterable){
+    //P => string or array of  integers
+    //R => array
+    
+    let result = []
+    for(let i=0; i<iterable.length; i++){
+      if(iterable[i] !== iterable[i+1]){
+        result.push(iterable[i])
+      }
+    }
+  return result
+  }
+
