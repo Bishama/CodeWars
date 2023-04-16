363a3ef1be14c94cb1cefc66f39ecf47af8422b3
Bishama Irfan <bir001@un-ihe.org>
2023-04-16 22:12:58 +0200

    intial commit

---
+++ 7th_Kyu/OddCubedNum.js
@@ -0,0 +1,18 @@
+/* Find the sum of the odd numbers within an array, after cubing the initial integers. The function 
+should return undefined if any of the values aren't numbers. */
+
+function sumOfCubedOdds(arr) {
+    let sum = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== 'number') {
+        return undefined; 
+      }
+  
+      if (arr[i] % 2 !== 0) {
+        sum += Math.pow(arr[i], 3); 
+      }
+    }
+  
+    return sum;
+  }
