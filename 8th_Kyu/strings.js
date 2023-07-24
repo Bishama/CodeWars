5b75f6c365799b8b47c7f81f377bf972f240f6c5
Bishama Irfan <bishamairfann10@gmail.com>
2023-07-24 20:37:23 +0200

    initial commit

---
+++ 8th_Kyu/strings.js
@@ -0,0 +1,6 @@
+/* Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name. */
+
+function combineNames(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+    return fullName;
+} 
