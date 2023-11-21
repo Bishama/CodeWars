0f61f0180803f2432e61e1125f9aa4bd2a2e9cc4
Bishama Irfan <bishamairfann10@gmail.com>
2023-11-21 10:26:24 +0100

    Compare two objects funciton

--- 7th_Kyu/ObjectsDeepEqual.js
+++ 7th_Kyu/ObjectsDeepEqual.js
@@ -4,16 +4,28 @@
 
 
 
+ function deepEqual(a, b) {
+    // Step 1: Direct comparison for non-objects.
+    if (a === b) return true;
+    if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) 
+        return false;
 
+    // Step 2: Compare property names.
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+    if (keysA.length !== keysB.length) return false;
 
+    // Step 3: Compare property values.
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) 
+            return false;
+    }
 
+    // Step 4: All checks passed, objects are deeply equal.
+    return true;
+}
 
-
-
-//let obj = {here: {is: "an"}, object: 2};
-//console.log(deepEqual(obj, obj));
-// → true
-//console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
+// Testing the function
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj)); // true
+console.log(deepEqual(obj, { here: 1, object: 2 })); // false
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 })); // true
