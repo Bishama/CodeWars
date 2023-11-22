1be6775a8d16fdfe68e09e5b658466613a1d7c86
Bishama Irfan <bishamairfann10@gmail.com>
2023-11-22 10:20:30 +0100

    Compare two objects

---
+++ 7th_Kyu/ObjectsDeepEqual.js
@@ -0,0 +1,19 @@
+
+/*Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same
+ properties, where the values of the properties are equal when compared with a recursive call to deepEqual. */
+
+
+
+
+
+
+
+
+
+//let obj = {here: {is: "an"}, object: 2};
+//console.log(deepEqual(obj, obj));
+// → true
+//console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
