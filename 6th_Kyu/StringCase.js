260b3a1034c7d8a0b559c36b0127eeebf0611088
Bishama Irfan <bishamairfann10@gmail.com>
2025-01-10 12:36:47 +0100

    add function to alternate character cases in a string

--- 6th_Kyu/StringCase.js
+++ 6th_Kyu/StringCase.js
@@ -1,4 +1,8 @@
-/* */
+/* Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:*/
 
 function toWeirdCase(string){
     //P => string
@@ -7,18 +11,30 @@ function toWeirdCase(string){
     
     //Initialize resulting string
     let res = ''
+    let counter = 0
     
     for(let i=0; i<string.length; i++){
       let char = string[i]
-      //If index is 0 or even, add upperase version of character to the result
-      if (i%2 == 0 || i == 0){
-        res+=char.toUpperCase()
+      //Reset the counter if there is space
+      if(char === ' '){
+        //Add space to the result
+        res+=char
+        counter = 0
       }
-      //Otherwise add the lowerCase version of that character
       else{
-        res+=char.toLowerCase()
-      }
+        //If index is 0 or even, add upperase version of character to the result
+        if (counter%2 === 0){
+        res+=char.toUpperCase()
+        }
+        //Otherwise add the lowerCase version of that character
+          else{
+            res+=char.toLowerCase()
+            }
+        counter++   // Increase counter only for non space characters
     }
+        
+      }
+      
     console.log(res)
     return res
   }
