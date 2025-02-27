117faa3d3e461ada827d9d3db29feb84f075c31f
Bishama Irfan <bishamairfann10@gmail.com>
2025-02-27 12:30:09 +0100

    initial commit

---
+++ 7th_Kyu/Currying.js
@@ -0,0 +1,9 @@
+/* make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.*/
+
+function multiplyAll(arr) {
+    return function(int){
+      return arr.map( num => num*int )
+    }
+  }
